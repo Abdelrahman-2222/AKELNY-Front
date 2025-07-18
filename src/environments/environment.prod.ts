05f9f6f146c5fde4704e2272ee0ae95e8fabdac2
Abdelrahman Hossam Eldin Zaki Ahmed <abdelrahmanhossam2222@gmail.com>
2025-07-18 11:36:59 +0300

    u

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://akelni.tryasp.net',
+  apiUrl: 'https://akelni.tryasp.net',
   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
   redirectUri: 'http://akelni.tryasp.net/auth/callback'
   // ,
