d3da099eeb8c95e7dd1ea36af758ebe97519131f
Abdelrahman Hossam Eldin Zaki Ahmed <abdelrahmanhossam2222@gmail.com>
2025-07-18 12:14:28 +0300

    yarab

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,22 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://akelni.tryasp.net',
+//   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
+//   redirectUri: 'https://akelni.tryasp.net/auth/callback'
+//   // ,
+//   // facebookAppId: 'YOUR_PRODUCTION_FACEBOOK_APP_ID'
+// };
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://akelni.tryasp.net/api',
+//   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
+//   redirectUri: 'https://akelni.tryasp.net/auth/callback'
+// };
+
 export const environment = {
   production: true,
-  apiUrl: 'https://akelni.tryasp.net',
+  apiUrl: 'https://akelni.tryasp.net/api',
   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
-  redirectUri: 'https://akelni.tryasp.net/auth/callback'
-  // ,
-  // facebookAppId: 'YOUR_PRODUCTION_FACEBOOK_APP_ID'
+  redirectUri: 'https://akelny-front.vercel.app/auth/callback'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,14 @@
 //   // facebookAppId: 'YOUR_FACEBOOK_APP_ID'
 // };
 
-// src/environments/environment.ts
+// // src/environments/environment.ts
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://akelni.tryasp.net/api' // Ensure this uses https
+// };
+
 export const environment = {
   production: false,
-  apiUrl: 'https://akelni.tryasp.net/api' // Ensure this uses https
+  apiUrl: 'https://localhost:7045/api', // or your local HTTPS URL
+  googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com'
 };
