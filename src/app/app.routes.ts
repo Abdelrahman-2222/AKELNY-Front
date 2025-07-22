b5b91f32d01c43285523f2f5257810a4dfb1621b
ahmedsalama0 <130906715+ahmedsalama0@users.noreply.github.com>
2025-07-22 23:07:36 +0300

    modifing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,19 @@ export const routes: Routes = [
     path: 'app',
     component: App,
   },
-  // { path: '', redirectTo: '/register', pathMatch: 'full' },
-  // {
-  //   path: 'register',
-  //   loadComponent: () => import('./auth/register/register').then(m => m.Register)
-  // },
-  // {
-  //   path: 'login',
-  //   loadComponent: () => import('./auth/login/login').then(m => m.Login)
-  // },
-  // {
-  //   path: 'main',
-  //   loadComponent: () => import('./home/main/main').then(m => m.Main)
-  // },
-  // { path: '**', redirectTo: '/register' }
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register').then((m) => m.Register),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./home/main/main').then((m) => m.Main),
+  },
+  { path: '**', redirectTo: '/register' },
 ];
