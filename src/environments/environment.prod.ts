47a02ab36929da46cb65e9f2ade2e140df723dd8
Abdelrahman Hossam Eldin Zaki Ahmed <abdelrahmanhossam2222@gmail.com>
2025-07-24 21:58:59 +0300

    vercel - auth issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,7 +16,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://akelni.tryasp.net/api',
+  apiUrl: '/api', // Use relative path - Vercel will proxy to your HTTP backend
   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
   redirectUri: 'https://akelny-front.vercel.app/auth/callback'
 };
