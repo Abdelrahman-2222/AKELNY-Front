824bb6362de80b8d68fb3ca3b99e4f7f01c5447a
Abdelrahman Hossam Eldin Zaki Ahmed <abdelrahmanhossam2222@gmail.com>
2025-08-23 05:56:50 +0300

    Try to make http serve as https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,18 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: '/api',
+//   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
+//   redirectUri: 'https://akelny-front.vercel.app/auth/callback',
+//   uploadsUrl: '/uploads',
+//   signalrUrl: ''
+// };
+
+// src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://akelni.tryasp.net/api', // Try HTTPS first
   googleClientId: '811631518025-je1km7ums6m720ct9v09vgvjtgr2qb24.apps.googleusercontent.com',
   redirectUri: 'https://akelny-front.vercel.app/auth/callback',
-  uploadsUrl: '/uploads',    
-  signalrUrl: ''
+  uploadsUrl: 'https://akelni.tryasp.net',
+  signalrUrl: 'https://akelni.tryasp.net' // Try HTTPS
 };
